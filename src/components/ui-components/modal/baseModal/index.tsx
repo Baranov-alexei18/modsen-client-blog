e3539f14687d2e8ac80d26d786469d63e4649d40
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-12 21:12:06 +0300

    feat(create adaptive videomodal component): create BaseModal and VideoModal components, added video

---
+++ src/components/ui-components/modal/baseModal/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+
+import { ButtonApp } from '../../button';
+import { ModalProps } from '../types';
+
+import styles from '../styles.module.scss';
+
+export const BaseModal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isOpen]);
+
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (modalRef.current === e.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className={`${styles.modal} ${isOpen ? styles.open : styles.closed}`}
+      ref={modalRef}
+      onClick={handleOverlayClick}
+      aria-hidden
+    >
+      <div className={styles.modalContent}>
+        <div className={styles.header}>
+          <span>Present Video</span>
+          <ButtonApp onClick={onClose}>Закрыть</ButtonApp>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
