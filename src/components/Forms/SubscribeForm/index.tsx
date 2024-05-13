dac69d9cef2eaa21b3c65bbfca4cc3592a1b1401
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-13 23:14:19 +0300

    feat(create send message to email): added email.js and updates subscriveForms component

--- src/components/Forms/SubscribeForm/index.tsx
+++ src/components/Forms/SubscribeForm/index.tsx
@@ -1,4 +1,6 @@
 import { SetStateAction, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import emailjs from '@emailjs/browser';
 
 import { ButtonApp } from '@/components/ui-components/button';
 import { InputApp } from '@/components/ui-components/input';
@@ -12,13 +14,48 @@ export const SubscribeForm = () => {
     setEmail(e.target.value);
   };
 
+  const templateParams = {
+    name: 'Alex',
+    to_email: email,
+  };
+
+  const sendEmail = () => {
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        templateParams,
+        {
+          publicKey: process.env.NEXT_PUBLIC_EMAILJS_KEY!,
+        },
+      )
+      .then(
+        () => {
+          setEmail('');
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <form className={styles.wrapper}>
+    <form className={styles.wrapper} onSubmit={sendEmail}>
       <span className={styles.info}>
         Subscribe to our news letter to get latest updates and news
       </span>
-      <InputApp placeholder="Enter Your Email" onChange={getToSubscribeEmail} />
-      <ButtonApp backgroundColor="var(--color-yellow)"> Subscribe</ButtonApp>
+      <InputApp
+        value={email}
+        placeholder="Enter Your Email"
+        onChange={getToSubscribeEmail}
+      />
+      <ButtonApp
+        backgroundColor="var(--color-yellow)"
+        onClick={sendEmail}
+      >
+        Subscribe
+      </ButtonApp>
     </form>
   );
 };
--- src/components/ui-components/input/index.tsx
+++ src/components/ui-components/input/index.tsx
@@ -3,12 +3,14 @@ import { ReactNode } from 'react';
 import styles from './styles.module.scss';
 
 type InputType = {
-    placeholder: string,
-    onChange: (e: { target: { value: string }}) => void
+  value: string,
+  placeholder: string,
+  onChange: (e: { target: { value: string } }) => void
 }
 
-export const InputApp = ({ placeholder, onChange }: InputType) => (
+export const InputApp = ({ value, placeholder, onChange }: InputType) => (
   <input
+    value={value}
     className={styles.wrapper}
     placeholder={placeholder}
     onChange={(e) => onChange(e)}
