0ed8d5e651277e523f09ce0225e3b5dcaab81d97
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-24 16:15:05 +0300

    feat(added style for mobile on category page): update styles

--- src/app/[locale]/category/[slug]/page.tsx
+++ src/app/[locale]/category/[slug]/page.tsx
@@ -3,11 +3,9 @@ import { useTranslations } from 'next-intl';
 import { HeaderCategory } from '../section/header';
 import { ListPosts } from '../section/posts';
 
-import styles from '../styles.module.scss';
+import styles from './styles.module.scss';
 
 export default function Category({ params }: { params: { slug: string } }) {
-  // const t = useTranslations('pages.privacyPolice');
-
   return (
     <main className={styles.wrapper}>
       <HeaderCategory slug={params.slug} />
