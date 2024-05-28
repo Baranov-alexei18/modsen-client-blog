c5be754b9d10dbf90c89493abcb96734af19d3f0
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-28 08:17:30 +0300

    refactor(update dinamiv imports): in home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,12 +8,24 @@ import { Banner } from './home/banner';
 
 import styles from './styles.module.scss';
 
-const SectionPost = dynamic(() => import('./home/section/posts'));
-const SectionAboutUs = dynamic(() => import('./home/section/about-us'));
-const SectionAuthor = dynamic(() => import('./home/section/authors'));
-const SectionCategory = dynamic(() => import('./home/section/category'));
-const SectionStarted = dynamic(() => import('./home/section/started'));
-const SectionTestimonial = dynamic(() => import('./home/section/testimonials'));
+const SectionPost = dynamic(() => import('./home/section/posts'), {
+  ssr: false,
+});
+const SectionAboutUs = dynamic(() => import('./home/section/about-us'), {
+  ssr: false,
+});
+const SectionAuthor = dynamic(() => import('./home/section/authors'), {
+  ssr: false,
+});
+const SectionCategory = dynamic(() => import('./home/section/category'), {
+  ssr: false,
+});
+const SectionStarted = dynamic(() => import('./home/section/started'), {
+  ssr: false,
+});
+const SectionTestimonial = dynamic(() => import('./home/section/testimonials'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
