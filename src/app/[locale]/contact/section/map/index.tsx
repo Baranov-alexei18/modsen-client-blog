48b038f5de3f05978553ceeb529e771b2f2c8e81
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-29 17:12:34 +0300

    refactor(update mapbox for vercel): update position accessToken

--- src/app/[locale]/contact/section/map/index.tsx
+++ src/app/[locale]/contact/section/map/index.tsx
@@ -5,35 +5,33 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import styles from './styles.module.scss';
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
-
 export const MapSection = () => {
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
+
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef(null);
 
   useEffect(() => {
-    if (typeof window === 'undefined' || map.current) return;
-    if (mapContainer.current) {
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [20, 55],
-        zoom: 3.5,
-      });
+    if (!mapboxgl.accessToken || typeof window === 'undefined' || map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [20, 55],
+      zoom: 3.5,
+    });
 
-      const markers = [
-        { lng: 27.59249, lat: 53.93432, name: 'Minsk, Belarus' },
-        { lng: 37.61937, lat: 55.76935, name: 'Moscow, Russia' },
-        { lng: 26.69731, lat: 58.36666, name: 'Tartu, Estonia' },
-      ];
+    const markers = [
+      { lng: 27.59249, lat: 53.93432, name: 'Minsk, Belarus' },
+      { lng: 37.61937, lat: 55.76935, name: 'Moscow, Russia' },
+      { lng: 26.69731, lat: 58.36666, name: 'Tartu, Estonia' },
+    ];
 
-      markers.forEach((marker) => {
-        new mapboxgl.Marker()
-          .setLngLat([marker.lng, marker.lat])
-          .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(marker.name))
-          .addTo(map.current!);
-      });
-    }
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(marker.name))
+        .addTo(map.current!);
+    });
   }, []);
 
   return <div className={styles.wrapper} ref={mapContainer} />;
