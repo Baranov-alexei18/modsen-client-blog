4449a9544773361815a0aefde3d5778207cafbff
Alexei Baranov <alexei-baranov18@mail.ru>
2024-06-03 15:19:44 +0300

    feat(delete data-testid from prod build): update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties: { properties: ['data-testid'] },
+  },
+};
 
 export default withNextIntl(nextConfig);
